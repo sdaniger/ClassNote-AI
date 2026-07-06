4d2f23963dc3ceb79c42c0efc9b88ea7d751d7ed
root <root@localhost.localdomain>
2026-07-06 21:35:16 +0900

    fix: inline shared code to avoid Metro bundler path alias issues

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -3,11 +3,6 @@ const path = require("path");
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.alias = {
-  "@": path.resolve(__dirname, "src"),
-  "@shared": path.resolve(__dirname, "../../shared"),
-};
-
 config.watchFolders = [
   ...config.watchFolders,
   path.resolve(__dirname, "../../shared"),
